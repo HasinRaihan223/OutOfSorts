924daa6962e1f305eef43fd6bc368e101a5476b0
HasinRaihan223 <hraihan20@stuy.edu>
2020-12-17 13:04:42 -0500

    Started SelectionSort

--- Sorts.java
+++ Sorts.java
@@ -11,4 +11,24 @@ public class Sorts{
         }
       }
   }
+
+  public static void selectionSort(int[] data){
+    int l = data.length;
+    for (int i = 0; i < l-1; i++){
+      for (int j = i+1; j < l; j++){
+        int current = i;
+        if (data[j] < data[current]){
+          current = j;
+        }
+        int temp = data[current];
+        data[current] = data[i];
+        data[i] = temp;
+      }
+    }
+  }
+
+  public static void insertionSort(int[] data){
+
+  }
+
 }
